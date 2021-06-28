91dd03e579abd4a34b7d26d74e04a6102a8f8da6
mounika1719 <71956696+mounika1719@users.noreply.github.com>
2021-06-28 16:33:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
-
 import { GlobalProvider } from './context/GlobalState';
-
 import './App.css';
-
 function App() {
   return (
     <GlobalProvider>
