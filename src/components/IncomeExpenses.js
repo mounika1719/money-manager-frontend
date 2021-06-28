f8ca902d7e3952d2c56c742f386f0bf128c2a0d5
mounika1719 <71956696+mounika1719@users.noreply.github.com>
2021-06-28 16:31:50 +0530

    Update IncomeExpenses.js

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,7 +1,5 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
-
-
 function moneyFormatter(num) {
   let p = num.toFixed(2).split('.');
   return (
@@ -19,18 +17,14 @@ function moneyFormatter(num) {
 
 export const IncomeExpenses = () => {
   const { transactions } = useContext(GlobalContext);
-
   const amounts = transactions.map(transaction => transaction.amount);
-
   const income = amounts
     .filter(item => item > 0)
     .reduce((acc, item) => (acc += item), 0);
-
   const expense = (
     amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
     -1
   );
-
   return (
     <div className="inc-exp-container">
         <div>
